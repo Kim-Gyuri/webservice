b89e7ec4e9d414a6748e9740287e1bdd0045bdfd
Kim-Gyuri <gyuring0203@gmail.com>
2021-12-03 19:20:03 +0900

    기본적인 구글 로그인, 로그아웃, 회원가입, 권한관리 기능 구현

--- src/main/java/hello/webservice/web/IndexController.java
+++ src/main/java/hello/webservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package hello.webservice.web;
 
+import hello.webservice.config.auth.dto.SessionUser;
 import hello.webservice.service.PostsService;
 import hello.webservice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +10,23 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -38,4 +47,5 @@ public class IndexController {
         postsService.delete(id);
         return id;
     }
+
 }
