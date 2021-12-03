b4a4a96eccf80c6e3cbc55ea66092b5bed7560e3
Kim-Gyuri <gyuring0203@gmail.com>
2021-12-04 01:23:52 +0900

    어노테이션 기반으로 개선하기

--- src/main/java/hello/webservice/web/IndexController.java
+++ src/main/java/hello/webservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package hello.webservice.web;
 
+import hello.webservice.config.auth.LoginUser;
 import hello.webservice.config.auth.dto.SessionUser;
 import hello.webservice.service.PostsService;
 import hello.webservice.web.dto.PostsResponseDto;
@@ -20,10 +21,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
